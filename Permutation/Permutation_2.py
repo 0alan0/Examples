0656bbfd00f2d22ce64358f56bd6eb3a989d0d11
AlanSS <alan.song@qq.com>
2019-01-04 16:18:00 +0800

    Create Permutation_2.py
    10位数枚举

---
+++ Permutation/Permutation_2.py
@@ -0,0 +1,26 @@
+def main():
+    a, b, c, d, e, f, g, h, i = 0, 0, 0, 0, 0, 0, 0, 0, 0
+    x = 10
+    for a in range(0, x):
+        # for b in range(0, x):
+            # for c in range(0, x):
+            #     for d in range(0, x):
+            #         for e in range(0, x):
+            #             for f in range(0, x):
+            #                 for g in range(0, x):
+            #                     for h in range(0, x):
+            #                         for i in range(0, x):
+                                        print(a, b, c, d, e, f, g, h, i)
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
